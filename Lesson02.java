52f67df0c94dfad46b052194129e10978f15a8cc
Shota Okugawa <okusho721@gmail.com>
2021-03-16 17:31:46 +0900

    Lesson03

--- Lesson02.java
+++ Lesson02.java
@@ -1,9 +1,19 @@
 class Lesson02 {
   public static void main(String[] args) {
-    int var;
+    int var = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
 
-    var = 1;
+    // int var-01 = 5;
+    // int 01var = 6;
 
     System.out.println(var);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
+    // System.out.println(var-01);
+    // System.out.println(01var);
+
   }
 }
