6e8d5d0359fea156145898243665854d33238d83
Shota Okugawa <okusho721@gmail.com>
2021-03-16 17:42:31 +0900

    Lesson3 Fin

--- Lesson03.java
+++ Lesson03.java
@@ -5,10 +5,26 @@ class Lesson03 {
     int var03 = 1234567890;
     long var04 = 123456789000L;
 
+    double var05 = 1.2345678;
+    float var06 = 1.234F;
+
+    char var07 = 'c';
+    String var08 = "hello, World!";
+
+    int var_a = 10;
+    int var_b = 1;
+    boolean var_bool;
+
+    var_bool = (var_a > var_b);
+
     System.out.println(var01);
     System.out.println(var02);
     System.out.println(var03);
     System.out.println(var04);
-
+    System.out.println(var05);
+    System.out.println(var06);
+    System.out.println(var07);
+    System.out.println(var08);
+    System.out.println(var_bool);
   }
 }
