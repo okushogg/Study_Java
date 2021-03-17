abea5f638cd0b2aa33d5736a52eb9f6a1a776bd9
Shota Okugawa <okusho721@gmail.com>
2021-03-17 09:45:33 +0900

    Lesson04 Finished

---
+++ Lesson04.java
@@ -0,0 +1,12 @@
+class Lesson04 {
+  public static void main(String[] args) {
+    String[] arr = { "sato", "suzuki", "tanaka" };
+
+    arr[1] = "okugawa";
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+
+  }
+}
