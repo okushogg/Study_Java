48c534b8cdc253626f3d28bf2b728864f5ad2694
Shota Okugawa <okusho721@gmail.com>
2021-03-19 22:15:39 +0900

    Lesson8-2

--- Lesson08.java
+++ Lesson08.java
@@ -26,12 +26,22 @@ public class Lesson08 {
   public static void main(String[] args) {
     Student a001 = new Student();
     a001.name = "sato";
-    double[] data = { 70, 65, 50, 10, 30 };
+    double[] data = { 40, 65, 50, 10, 30 };
 
     double avg = a001.calculateAvg(data);
     String result = a001.judge(avg);
 
     System.out.println(avg);
     System.out.println(a001.name + " => " + result);
+
+    Student a002 = new Student();
+    a002.name = "tanaka";
+    double[] data = { 80, 65, 55, 89, 50 };
+
+    double avg = a002.calculateAvg(data);
+    String result = a002.judge(avg);
+
+    System.out.println(avg);
+    System.out.println(a002.name + " => " + result);
   }
 }
